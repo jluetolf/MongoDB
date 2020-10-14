032476aceff26856792dd280499344498a914536
jluetolf <joerg_luetolf@hotmail.com>
2020-10-14 10:09:51 +0200

    -

--- src/main/java/ch/sbb/mongodb/repository/TripRepository.java
+++ src/main/java/ch/sbb/mongodb/repository/TripRepository.java
@@ -1,12 +1,7 @@
 package ch.sbb.mongodb.repository;
 
-import ch.sbb.mongodb.objects.Customer;
 import ch.sbb.mongodb.objects.Trip;
-import ch.sbb.mongodb.objects.Trips;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
 
 public interface TripRepository extends MongoRepository<Trip, Long> {
 }
